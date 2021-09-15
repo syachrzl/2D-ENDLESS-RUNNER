5290775c59578d1a31e8857ce189d233ba743300
Syah Rizal Fauzy <syachrzl@gmail.com>
2021-09-15 15:04:24 +0700

    FIRST - 2D ENDLESS RUNNER

---
+++ Assets/Scripts/CharacterSoundController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSoundController : MonoBehaviour
+{
+    public AudioClip jump;
+    public AudioClip scoreHighlight;
+
+    private AudioSource audioPlayer;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        audioPlayer = GetComponent<AudioSource>();
+    }
+
+    public void PlayScoreHighlight()
+    {
+        audioPlayer.PlayOneShot(scoreHighlight);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void PlayJump()
+    {
+        audioPlayer.PlayOneShot(jump);
+    }
+}
